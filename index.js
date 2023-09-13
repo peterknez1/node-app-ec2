5191d0b92e1a4d9351e1aafa5d0411871ba23b86
peterknez1 <mac@proxima.si>
2023-09-13 13:17:01 +0200

    initial

---
+++ index.js
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const app = express();
+
+app.listen(3000, () => console.log('Api running on port 3000.'));
+
+app.get('/', (req, res) => res.json('My API running'));
