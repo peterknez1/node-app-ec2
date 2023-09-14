aa8ef9cecac4ae4507586c9b101d5ea6acf3b3ee
peterknez1 <mac@proxima.si>
2023-09-14 09:38:46 +0200

    sql database connection

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 import express from 'express';
 
 const app = express();
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'proximapassword',
+  database: ''
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to the database');
+});
 
 app.listen(3000, () => console.log('Api running on port 3000.'));
 
